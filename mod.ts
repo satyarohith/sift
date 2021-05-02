bef29e459c2196fbe86a8952b485558eb216fc12
ngruychev <nickick@nickick.tk>
2021-05-02 03:27:31 +0300

    fix: headers are immutable error
    closes #14

--- mod.ts
+++ mod.ts
@@ -57,6 +57,17 @@ export function serve(userRoutes: Routes): void {
   });
 }
 
+async function newResponse(res: Response, headers: Record<string, string>): Promise<Response> {
+    return new Response(await res.blob(), {
+        headers: {
+            ...Object.fromEntries(res.headers.entries()),
+            ...headers,
+        },
+        status: res.status,
+        statusText: res.statusText,
+    });
+}
+
 async function handleRequest(
   request: Request,
   routes: Routes,
@@ -86,7 +97,7 @@ async function handleRequest(
         }
       }
     } else {
-      response.headers.set("x-function-cache-hit", "true");
+      response = await newResponse(response, { "x-function-cache-hit": "true" });
     }
 
     // return not found page if no handler is found.
@@ -173,7 +184,7 @@ export function serveStatic(
       }
 
       const cType = contentType(String(lookup(filePath)));
-      if (cType) response.headers.set("content-type", cType);
+      if (cType) response = await newResponse(response, { "content-type": cType });
 
       if (typeof intervene === "function") {
         response = await intervene(request, response);
@@ -190,7 +201,7 @@ export function serveStatic(
         response = (await globalCache.match(request)) as Response;
       }
     } else {
-      response.headers.set("x-function-cache-hit", "true");
+      response = await newResponse(response, { "x-function-cache-hit": "true" });
     }
 
     return response;
