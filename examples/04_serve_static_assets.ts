cdcd0b9ca5d504072c7eb7d13fbbfadd57187032
Satya Rohith <me@satyarohith.com>
2021-10-24 04:27:27 +0530

    fix(serveStatic): stop rewriting file as directory

--- examples/04_serve_static_assets.ts
+++ examples/04_serve_static_assets.ts
@@ -5,6 +5,9 @@ serve({
   "/blog/:slug": (_request, params) => {
     return new Response(`You visited /${params?.slug}`);
   },
+  "/about": serveStatic("../readme.md", {
+    baseUrl: import.meta.url,
+  }),
   // The path should end with `filename+` for serveStatic to
   // construct correct URL to the requested resource.
   // The below path will serve the root of the repository.
